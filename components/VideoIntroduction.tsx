3fb0770dd93f0268b5c912ac67d1982b8f9a5e25
AnimeshA867 <animeshacharya867+github@gmail.com>
2024-12-28 17:57:46 +0545

    Enhance VideoIntroduction component: add player configuration for YouTube video preload and remove outdated documentation

--- components/VideoIntroduction.tsx
+++ components/VideoIntroduction.tsx
@@ -9,22 +9,6 @@ import Loading from "./Loading";
 
 interface VideoIntroductionProps {}
 
-/**
- * VideoIntroduction component renders a video player inside a responsive container.
- *
- * @component
- * @param {VideoIntroductionProps} props - The props for the VideoIntroduction component.
- *
- * @returns {JSX.Element} A JSX element containing a video player.
- *
- * @example
- * <VideoIntroduction />
- *
- * @remarks
- * The video player is wrapped in a Suspense component to handle lazy loading.
- * The video URL is hardcoded to a specific YouTube video.
- * The container has responsive width and aspect ratio to maintain the video dimensions.
- */
 const VideoIntroduction: FC<VideoIntroductionProps> = ({}) => {
   return (
     <div className="w-4/5 aspect-video mx-auto relative">
@@ -35,6 +19,11 @@ const VideoIntroduction: FC<VideoIntroductionProps> = ({}) => {
             url="https://www.youtube.com/watch?v=UvqdUkqK5g0"
             width="100%"
             height="100%"
+            config={{
+              youtube: {
+                playerVars: { preload: "metadata" },
+              },
+            }}
           />
         </Suspense>
       </div>
